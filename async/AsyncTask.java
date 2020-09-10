173e6588fd37867ff996a31114c1108d97084b3d
Johny Da Developer <johnydadeveloper@gmail.com>
2020-09-11 01:54:50 +0200

    Updated, no activity required now

--- async/AsyncTask.java
+++ async/AsyncTask.java
@@ -1,45 +1,114 @@
-package cz.johnyapps.pocketprague.async;
+package cz.barda.jidelna.async;
 
-import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
 
-public abstract class AsyncTask<INPUT, OUTPUT> {
-    private AsyncWorker asyncWorker;
+public abstract class AsyncTask<INPUT, PROGRESS, OUTPUT> {
+    private boolean cancelled = false;
 
-    public AsyncTask(AsyncWorker asyncWorker) {
-        this.asyncWorker = asyncWorker;
+    public AsyncTask() {
+
     }
 
+    /**
+     * Starts is all
+     * @param input Data you want to work with in the background
+     */
     public void execute(final INPUT input) {
-        if (asyncWorker.isDestroyed()) {
-            onFailure(new Exception("AsyncWorker already destroyed"));
-            return;
-        }
-
         onPreExecute();
 
-        asyncWorker.execute(new Callable<OUTPUT>() {
-            @Override
-            public OUTPUT call() throws Exception {
-                return doInBackground(input);
-            }
-        }, new AsyncWorker.OnTaskCompletedListener() {
-            @Override
-            public void onComplete(Object object) {
-                onPostExecute((OUTPUT) object);
-            }
+        ExecutorService executorService = AsyncWorker.getInstance().getExecutorService();
+        executorService.execute(() -> {
+            try {
+                final OUTPUT output = doInBackground(input);
+                AsyncWorker.getInstance().getHandler().post(() -> onPostExecute(output));
+            } catch (final Exception e) {
+                e.printStackTrace();
 
-            @Override
-            public void onTaskInterrupted(Exception e) {
-                onFailure(e);
+                AsyncWorker.getInstance().getHandler().post(() -> onBackgroundError(e));
             }
         });
     }
 
-    public abstract void onPreExecute();
+    /**
+     * Call to publish progress from background
+     * @param progress  Progress made
+     */
+    protected void publishProgress(final PROGRESS progress) {
+        AsyncWorker.getInstance().getHandler().post(() -> {
+            if (onProgressListener != null) {
+                onProgressListener.onProgress(progress);
+            }
+        });
+    }
 
-    public abstract OUTPUT doInBackground(INPUT inputs);
+    /**
+     * Call to cancel background work
+     */
+    public void cancel() {
+        cancelled = true;
+    }
 
-    public abstract void onPostExecute(OUTPUT output);
+    /**
+     *
+     * @return Returns true if the background work should be cancelled
+     */
+    protected boolean isCancelled() {
+        return cancelled;
+    }
 
-    public abstract void onFailure(Exception e);
+    /**
+     * Call this method after cancelling background work
+     */
+    protected void onCancelled() {
+        AsyncWorker.getInstance().getHandler().post(() -> {
+            if (onCancelledListener != null) {
+                onCancelledListener.onCancelled();
+            }
+        });
+    }
+
+    /**
+     * Work which you want to be done on UI thread before {@link #doInBackground(Object)}
+     */
+    protected abstract void onPreExecute();
+
+    /**
+     * Work on background
+     * @param input Input data
+     * @return      Output data
+     * @throws Exception    Any uncought exception which occurred while working in background. If
+     * any occurs, {@link #onBackgroundError(Exception)} will be executed (on the UI thread)
+     */
+    protected abstract OUTPUT doInBackground(INPUT input) throws Exception;
+
+    /**
+     * Work which you want to be done on UI thread after {@link #doInBackground(Object)}
+     * @param output    Output data from {@link #doInBackground(Object)}
+     */
+    protected abstract void onPostExecute(OUTPUT output);
+
+    /**
+     * Triggered on UI thread if any uncought exception occurred while working in background
+     * @param e Exception
+     * @see #doInBackground(Object)
+     */
+    protected abstract void onBackgroundError(Exception e);
+
+    private OnProgressListener<PROGRESS> onProgressListener;
+    public interface OnProgressListener<PROGRESS> {
+        void onProgress(PROGRESS progress);
+    }
+
+    public void setOnProgressListener(OnProgressListener<PROGRESS> onProgressListener) {
+        this.onProgressListener = onProgressListener;
+    }
+
+    private OnCancelledListener onCancelledListener;
+    public interface OnCancelledListener {
+        void onCancelled();
+    }
+
+    public void setOnCancelledListener(OnCancelledListener onCancelledListener) {
+        this.onCancelledListener = onCancelledListener;
+    }
 }
