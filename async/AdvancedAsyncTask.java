52b45e4049303d7d9642e2fe660e40d2a43fab00
Johny Da Developer <johnydadeveloper@gmail.com>
2020-09-23 14:57:15 +0200

    Added AdvancedAsyncTask

---
+++ async/AdvancedAsyncTask.java
@@ -0,0 +1,125 @@
+package cz.barda.jidelnacz2.async;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public abstract class AdvancedAsyncTask<INPUT, PROGRESS, OUTPUT> {
+    private boolean cancelled = false;
+    private Future<OUTPUT> outputFuture;
+
+    public AdvancedAsyncTask() {
+
+    }
+
+    /**
+     * Starts is all
+     * @param input Data you want to work with in the background
+     */
+    public void execute(final INPUT input) {
+        onPreExecute();
+
+        ExecutorService executorService = AsyncWorker.getInstance().getExecutorService();
+        outputFuture = executorService.submit(() -> {
+            try {
+                final OUTPUT output = doInBackground(input);
+                AsyncWorker.getInstance().getHandler().post(() -> onPostExecute(output));
+                return output;
+            } catch (Exception e) {
+                AsyncWorker.getInstance().getHandler().post(() -> onBackgroundError(e));
+                throw e;
+            }
+        });
+    }
+
+    public OUTPUT get() throws Exception {
+        return outputFuture.get();
+    }
+
+    public OUTPUT get(long timeout, TimeUnit timeUnit) throws Exception {
+        return outputFuture.get(timeout, timeUnit);
+    }
+
+    /**
+     * Call to publish progress from background
+     * @param progress  Progress made
+     */
+    protected void publishProgress(final PROGRESS progress) {
+        AsyncWorker.getInstance().getHandler().post(() -> {
+            if (onProgressListener != null) {
+                onProgressListener.onProgress(progress);
+            }
+        });
+    }
+
+    /**
+     * Call to cancel background work
+     */
+    public void cancel() {
+        cancelled = true;
+    }
+
+    /**
+     *
+     * @return Returns true if the background work should be cancelled
+     */
+    protected boolean isCancelled() {
+        return cancelled;
+    }
+
+    /**
+     * Call this method after cancelling background work
+     */
+    protected void onCancelled() {
+        AsyncWorker.getInstance().getHandler().post(() -> {
+            if (onCancelledListener != null) {
+                onCancelledListener.onCancelled();
+            }
+        });
+    }
+
+    /**
+     * Work which you want to be done on UI thread before {@link #doInBackground(Object)}
+     */
+    protected abstract void onPreExecute();
+
+    /**
+     * Work on background
+     * @param input Input data
+     * @return      Output data
+     * @throws Exception    Any uncought exception which occurred while working in background. If
+     * any occurs, {@link #onBackgroundError(Exception)} will be executed (on the UI thread)
+     */
+    protected abstract OUTPUT doInBackground(INPUT input) throws Exception;
+
+    /**
+     * Work which you want to be done on UI thread after {@link #doInBackground(Object)}
+     * @param output    Output data from {@link #doInBackground(Object)}
+     */
+    protected abstract void onPostExecute(OUTPUT output);
+
+    /**
+     * Triggered on UI thread if any uncought exception occurred while working in background
+     * @param e Exception
+     * @see #doInBackground(Object)
+     */
+    protected abstract void onBackgroundError(Exception e);
+
+    private OnProgressListener<PROGRESS> onProgressListener;
+    public interface OnProgressListener<PROGRESS> {
+        void onProgress(PROGRESS progress);
+    }
+
+    public void setOnProgressListener(OnProgressListener<PROGRESS> onProgressListener) {
+        this.onProgressListener = onProgressListener;
+    }
+
+    private OnCancelledListener onCancelledListener;
+    public interface OnCancelledListener {
+        void onCancelled();
+    }
+
+    public void setOnCancelledListener(OnCancelledListener onCancelledListener) {
+        this.onCancelledListener = onCancelledListener;
+    }
+}
